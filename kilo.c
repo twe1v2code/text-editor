5e3c3682c64f959ed1e5b18a73f2225e07f67b41
twe1v2code <6logan7@gmail.com>
2025-01-08 17:43:48 +0600

    Added some I/O flags

--- kilo.c
+++ kilo.c
@@ -17,7 +17,9 @@ void enableRawMode(void)
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | IEXTEN | ICANON | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -29,9 +31,9 @@ int main(void)
     char c;
     while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
         if(iscntrl(c)) 
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         else
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
     }
 
     return 0;
