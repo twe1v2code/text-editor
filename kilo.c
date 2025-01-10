fb84901f069075b1a6f30edb4cf5538719ff74ec
twe1v2code <6logan7@gmail.com>
2025-01-10 21:56:44 +0600

    Error logging

--- kilo.c
+++ kilo.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
-void disableRawMode(void)
-{
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode(void){
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode(void)
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -24,7 +30,7 @@ void enableRawMode(void)
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main(void)
@@ -33,7 +39,7 @@ int main(void)
 
     while(1){
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if(iscntrl(c)) 
             printf("%d\r\n", c);
         else
